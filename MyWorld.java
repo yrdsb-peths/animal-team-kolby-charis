5ec46357bb1526fdf79593349a28c587640cfabc
kolbyng <440021056@gapps.yrdsb.ca>
2025-12-04 14:05:03 -0500

    create elephant movement

--- MyWorld.java
+++ MyWorld.java
@@ -1,5 +1,12 @@
 import greenfoot.*;
 
+/**
+ * MyWorld class
+ * 
+ * @author Kolby
+ * @version 04/12/2025
+ */
+
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
