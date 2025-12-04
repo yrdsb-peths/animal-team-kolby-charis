8e712ec35ec91f59d2d3a4da4f2680ed9125760f
kolbyng <440021056@gapps.yrdsb.ca>
2025-12-04 14:09:32 -0500

    create apple

---
+++ Apple.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Apple actor
+ * 
+ * @author Kolby
+ * @version 04/12/2025
+ */
+public class Apple extends Actor
+{
+    public void act()
+    {
+        setLocation(getX(), getY() + 1);
+        if(isTouching(Elephant.class))
+        {
+            getWorld().removeObject(this);
+        }
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -10,5 +10,18 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        createApple();
+    }
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        addObject(apple, randomX(), 0);
+    }
+
+    public int randomX()
+    {
+        return Greenfoot.getRandomNumber(getWidth());
     }
 }
