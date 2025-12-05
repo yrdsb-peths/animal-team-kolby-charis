aa8466a293c4bd2898037a81095434f25ad290f8
kolbyng <440021056@gapps.yrdsb.ca>
2025-12-05 14:19:27 -0500

    initialize elephant sprite array

--- Elephant.java
+++ Elephant.java
@@ -8,6 +8,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    private GreenfootImage[] images;
+    private int imageIndex;
+    public Elephant
+    {
+        images = new GreenfootImage[8];
+        for (int i = 0; i < 8; i++)
+        {
+            images[i] = new GreenfootImage("idle" + i + ".png");
+        }
+        setImage(images[0]);
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("right"))
